df063d70f99cc59766132d3b3757da2a566aadbc
Adewale-tech <waliyullahadewale30@gmail.com>
2025-10-27 17:41:43 +0100

    Fix database connection parameters in database.py

--- app/database.py
+++ app/database.py
@@ -5,18 +5,18 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.getenv("${{RAILWAY_PRIVATE_DOMAIN}}")
+DB_HOST = os.getenv("mysql.railway.internal")
 DB_PORT = os.getenv("3306")
 DB_USER = os.getenv("root")
-DB_PASSWORD = os.getenv("${{MYSQL_ROOT_PASSWORD}}")
+DB_PASSWORD = os.getenv("zVpQebhYYstGVTbLNceobfGvwVovtuKN")
 DB_NAME = os.getenv("railway")
 
 DATABASE_URL = (
-    f"mysql+mysqlconnector://{railway}:{${{MYSQL_ROOT_PASSWORD}}}"
-    f"@{${{RAILWAY_PRIVATE_DOMAIN}}}:{3306}/{railway}"
+    f"mysql+mysqlconnector://{root}:{zVpQebhYYstGVTbLNceobfGvwVovtuKN}"
+    f"@{mysql.railway.internal}:{3306}/{railway}"
 )
 
-engine = create_engine(mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}})
+engine = create_engine(mysql://root:zVpQebhYYstGVTbLNceobfGvwVovtuKN@mysql.railway.internal:3306/railway)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
