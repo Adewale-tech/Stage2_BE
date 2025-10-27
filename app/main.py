4797444f277ae0477ff842199b72c9604cb34e47
Adewale-tech <waliyullahadewale30@gmail.com>
2025-10-27 17:27:07 +0100

    Implement CRUD operations for Country model and add database dependency in FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, engine
+from app import models, crud, schemas
+
+# DB Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "Stage 2 Backend Running"}
