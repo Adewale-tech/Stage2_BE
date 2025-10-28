1537e847448c7736415eed1c2ffcd0ce7e5d98ee
Adewale-Tech <waliyullahadewale30@gmail.com>
2025-10-28 16:52:36 +0100

    corrected  refactor from post entries

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routers import countries
+from app.routers.countries import router as countries_router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="Country API",
-    description="FastAPI + MySQL Railway Integration",
-    version="1.0.0"
-)
+app = FastAPI(title="Country API", description="FastAPI + MySQL Railway Integration", version="1.0.0")
 
-app.include_router(countries.router)
+app.include_router(countries_router, prefix="/countries", tags=["Countries"])
 
 @app.get("/")
 def root():
-    return {"message": "Country API Running ✅"}
+    return {"message": "Country API running successfully ✅"}
