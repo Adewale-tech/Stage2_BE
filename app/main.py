dd07270d686ad48f5bd0c2cde4b6c65b7612fdce
Adewale-Tech <waliyullahadewale30@gmail.com>
2025-10-28 14:42:59 +0100

    remove indent  router

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(countries.routers)
+app.include_routers(countries.router)
 
 @app.get("/")
 def root():
