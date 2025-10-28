68709a660a202c270243dfc2400f820fb8d52928
Adewale-Tech <waliyullahadewale30@gmail.com>
2025-10-28 15:28:30 +0100

    Refactor database.py to remove dotenv dependency, simplify DATABASE_URL assignment, and clean up unused code

--- app/database.py
+++ app/database.py
@@ -1,33 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./countries.db"
 
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    echo=True
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-DATABASE_URL = "sqlite:///./countries.db"
-
-engine = create_engine(mysql://root:DlOrweARmlgJfHvFswdVcLWgrUoAgJwa@tramway.proxy.rlwy.net:40552/railway, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
