abbedfcb361ae222deb6c7c903e151dc19bdb973
Adewale-Tech <waliyullahadewale30@gmail.com>
2025-10-29 19:08:10 +0100

    added base models to show table

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import countries
+from app import models
 
+
+models.Base.metadata.create_all(bind=engine)
 # Create DB tables
 Base.metadata.create_all(bind=engine)
 
