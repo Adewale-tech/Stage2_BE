1ab6f2e0fdf86ecc5bde542effb57c5ebbeaa7da
Adewale-Tech <waliyullahadewale30@gmail.com>
2025-10-29 18:58:32 +0100

    corrected function variable

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+ffrom sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -6,9 +6,13 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("mysql://root:DlOrweARmlgJfHvFswdVcLWgrUoAgJwa@tramway.proxy.rlwy.net:40552/railway")
+# Use your Railway URL directly (no os.getenv needed for now)
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:DlOrweARmlgJfHvFswdVcLWgrUoAgJwa@tramway.proxy.rlwy.net:40552/railway"
 
-engine = create_engine(mysql+pymysql://root:DlOrweARmlgJfHvFswdVcLWgrUoAgJwa@tramway.proxy.rlwy.net:40552/railway)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
